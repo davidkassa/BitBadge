9e3832fc2e68e44e19aa59f5afb53d41bd136988
maxburn <maxburn@gmail.com>
2014-02-15 15:33:56 -0600

    this is not broken, but the api is stubbed out for you.

--- modules/blockChainRequester.js
+++ modules/blockChainRequester.js
@@ -1,6 +1,7 @@
 
 var request = require('request');
-
+var ws = require('websocket').client;
+var client = new ws();
 
 exports.getTotalSent = function(address, callback) {
   request('http://blockchain.info/address/' + address + '?format=json', parse);
@@ -8,4 +9,37 @@ exports.getTotalSent = function(address, callback) {
   function parse(error, response, body) {
 	callback(JSON.parse(body)['total_sent']);
   }
+};
+
+exports.subscribeToAddress = function(address, callback) {
+    client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket client connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+
+    function sendNumber() {
+        if (connection.connected) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            connection.sendUTF(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+});
+    
+   client.connect('ws://ws.blockchain.info:8335/inv', 'echo-protocol'); 
+  
 };
