69f1464446ec7aba2a2fa6d0aadc1989a6beb17d
Jazmyn R <ZeWaPr@users.noreply.github.com>
2014-02-15 18:09:00 -0600

    Add script to generate assert files when user earns badge.

---
+++ modules/assertfilegen.py
@@ -0,0 +1,25 @@
+url_id = 2 #hardcoded because time
+
+make_assert_url():
+    url = “http://bitbadge.herokuapp.com/badges/” + url_id +“.json”
+    open(url)
+    #push this address to heroku
+    url_id++
+
+make_assert_file(uid, iden, time, badgeurl):
+    asserturl = make_assert_url() 
+    string = " {
+    "uid": "+ uid +",
+    "recipient": {
+    "type": "email",
+    "hashed": false,
+    "identity": "+ iden +"
+    },
+    "issuedOn": "+ time +",
+    "badge": "+ badgeurl +",
+    "verify": {
+    "type": "hosted",
+    "url": "+ asserturl +"
+    }
+    } "
+    asserturl.write(string)
